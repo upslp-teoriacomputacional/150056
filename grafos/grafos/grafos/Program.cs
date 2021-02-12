a212bdcb14f4ca5967c2cb0ca1a323f02d074a61
Isai Abimael Guevara Silva <150056@upslp.edu.mx>
2021-02-11 22:56:44 -0600

    Practica 2 grafos

---
+++ grafos/grafos/grafos/Program.cs
@@ -0,0 +1,49 @@
+﻿/*****************************************************************************************
+                        Universidad Politecnica de San Luis Potosí
+                        Ingeniería en Tecnologías de Información
+
+                        Materia:    Teoría Computacional
+                        Profesor:   Juan Carlos González Ibarra
+                        Nombre:     Guevara Silva Isaí Abimael
+                        Matricula:  150056
+
+                        C# program for validation of a graph
+*****************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafos {
+    //Se utiliza matriz de adyacencia para definición de grafo
+    class CGrafo {
+        int[,] mAdyacencia;
+        int[] indegree;
+        int nodos;
+
+        public CGrafo(int pNodos) {
+            nodos = pNodos;
+
+            //Instanciar matriz de adyacencia
+            mAdyacencia = new int[nodos, nodos];
+
+            //Instanciar arrelo indegree
+            indegree = new int[nodos];
+        }
+
+        public void AdicionaArista(int pNodoInicio, int pNodoFinal) {
+            mAdyacencia[pNodoInicio, pNodoFinal] = 1;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            int nodo = 0;
+
+            CGrafo miGrafo = new CGrafo(10);
+
+            miGrafo.AdicionaArista('a', 'c');
+        }
+    }
+}
